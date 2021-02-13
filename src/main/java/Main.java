538adc713f5a0af716a9497ef8763f4752082f60
Ildar Akhmetkhanov <akhmetkhanovis@gmail.com>
2021-02-14 01:05:18 +0500

    Mockito tests executed.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ import ru.netology.geo.GeoService;
 import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationService;
 import ru.netology.i18n.LocalizationServiceImpl;
-import ru.netology.sender.MessageSender;
-import ru.netology.sender.MessageSenderImpl;
+import ru.netology.MessageSender;
+import ru.netology.MessageSenderImpl;
 
 public class Main {
 
@@ -16,7 +16,7 @@ public class Main {
         LocalizationService localizationService = new LocalizationServiceImpl();
         MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
 
-        Map<String, String> headers = new HashMap<String, String>();
+        Map<String, String> headers = new HashMap<>();
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
         messageSender.send(headers);
     }
